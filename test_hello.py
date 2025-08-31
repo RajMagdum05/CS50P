8800c94066a66b4a26c821d14babcc15098fb6b7
Raj Magdum <rajmagdum6844@gmail.com>
2025-08-31 19:02:33 +0530

    Delete test_hello.py

--- test_hello.py
+++
@@ -1,7 +0,0 @@
-from hello import hello
-
-def test_hello():
-    assert hello("Raj") == "hello, Raj"
-    assert hello() == "hello, world"
-
-# so to do this hello function from hello.py should return and not print
